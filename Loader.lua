7860484bc06c84c96f7616f3ce1b123ecac0cc95
Lozsia <114638046+Lozsia@users.noreply.github.com>
2023-09-01 22:11:04 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,4 +1,4 @@
-if game.PlaceId == 1962086868 or game.PlaceId == 3582763398 or game.PlaceId == 5253186791
+if game.PlaceId == 1962086868 or game.PlaceId == 3582763398 or game.PlaceId == 5253186791 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Lozsia/Yum-Hub/main/Maps/Tower-of-Hell.lua"))();
 elseif game.PlaceId == 662417684 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Lozsia/Yum-Hub/main/Maps/LUCKY-BLOCKS-Battlegrounds.lua"))();
