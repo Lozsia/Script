41a78af4ab7e2ed2003a820fecaf8dc62404126e
asdv123zguysz <114638046+Lozsia@users.noreply.github.com>
2024-12-29 17:58:22 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,8 +1,8 @@
-local Url = "https://raw.githubusercontent.com/Lozsia/Script-Hub/refs/heads/main/Chart"
+local Url = "https://raw.githubusercontent.com/Lozsia/Script-Hub/refs/heads/main/Chart/"
 local GameId = {
     [662417684] = "Lucky-blocks-battlegrounds.lua",
 }
-for i, v in pairs(Id) do
+for i, v in pairs(GameId) do
     if i == game.PlaceId or i == game.GameId then
         loadstring(game:HttpGet(Url .. v))()
     end
